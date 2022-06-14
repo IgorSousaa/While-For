b1e03dc156817f8f436d2cee56e5160c13d1b26e
IgorSousaa <igoralvesnasc2@gmail.com>
2022-06-14 19:20:06 -0300

    Exercício de While e For

---
+++ script.js
@@ -0,0 +1,20 @@
+
+for(let numero = 1; numero <= 10; numero++){
+    console.log("Essa váriavel foi criado com For", numero);
+}
+
+let number = 10
+while(number >= 1){
+    console.log("Essa váriavel foi criado com While", number);
+    number--;
+}
+
+let impares = 1;
+while(impares <= 100){
+    console.log("Números Impares", impares);
+    impares += 2;
+}
+
+for(let pares = 0; pares <= 100; pares += 2){
+    console.log("Números Pares", pares);
+}
